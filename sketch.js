da9f0370821f0beddd49df45113dac7ebdf710ed
Kateygirl76 <10069@bwls.net>
2016-10-19 13:55:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ function setup() {
 
 function draw() {
   quad( mouseY , mouseX , 86  , 20 , 69 , 63 , 30 , 76 );
-  fill( 102 , 0 , 102 )
-  stroke( 153 , 102 , 255 );
+  fill( 255 , 255 , 204 )
+  stroke( 0 , 255 , 204 );
   triangle( mouseX , mouseY , 58 , 20 , 86 , 75 );
   stroke( 0 , 255 , 204 )
 }
